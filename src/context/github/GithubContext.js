ea42f31b54bc9bf879568163472865cf7cd38b70
kan1657 <kan1657@hotmail.com>
2022-02-01 15:30:37 +0700

    Get Single User

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
   const [state, dispatch] = useReducer(githubReducer, initialState)
@@ -31,6 +32,28 @@ export const GithubProvider = ({ children }) => {
       payload: items,
     })
   }
+
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
   // Clear users from state
   const clearUsers = () =>
     dispatch({
@@ -49,8 +72,10 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
